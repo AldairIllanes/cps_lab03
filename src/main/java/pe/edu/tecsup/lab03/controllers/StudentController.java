ee96febbefa36d3b552d5bd0e04cd27701902717
AldairIllanes <aldair.illanes@tecsup.edu.pe>
2026-09-04 20:17:16 -0500

    Modifique la calse StudenController V2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -3,3 +3,4 @@ package pe.edu.tecsup.lab03.controllers;
 public class StudentController {
 }
 //Modifique esta clase - Yeferson Rodrigo Quispe Alcasihuincha
+//Modifique esta clase - Aldair Junior Illanes Apaza.
