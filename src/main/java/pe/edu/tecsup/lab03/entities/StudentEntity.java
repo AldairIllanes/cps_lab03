3c808540d60b30e4f55af99bcf7cb57b87777fab
Nestor CQ <checcanestor@gmail.com>
2026-09-04 20:09:18 -0500

    Modifique StudentEntity y StudentRepository para sprint-2

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.entities;
-
+/**
+ * Modificado por: Néstor Checca
+ * Descripción: Actualización de la entidad para el sprint-2
+ */
 public class StudentEntity {
 }
--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.repositories;
-
+/**
+ * Modificado por: Néstor Checca
+ * Descripción: Actualización del repositorio para el sprint-2
+ */
 public class StudentRepository {
 }
